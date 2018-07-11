745c091e1dc5b312466d34d6c0e2df543c1672ab
RomanBeliy <romanpeople2292@yandex.ru>
2018-07-11 13:29:00 +0300

    add socket.io

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 const nunjucks = require('nunjucks');
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://locallhost:27017/chatic', {handleUseMongoClient: true});
+mongoose.Promise = require('bluebird');
 
 nunjucks.configure('../client/views', {
     autoescape: true,
@@ -15,17 +19,8 @@ app.use('/assets', express.static('../client/public'))
 app.get('/', (req, res)=> {
     res.render('index.html', {date: new Date()})
 });
-io.on('connection', function (socket) {
-    socket.emit('connected', "You connected");
-    socket.on('msg', content => {
-        const obj = {
-            date: new Date(),
-            content: content,
-            username: socket.id
-        };
-        socket.emit('message', obj);
-    })
-});
+
+require('./sockets')(io);
 
 server.listen(7777, '0.0.0.0', ()=> {
     console.log('Server started on port 7777')
